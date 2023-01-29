cf10761f8593c5ffe60907bfaf71197683e9fa34
Sergey <sergey-petric@yandex.ru>
2023-01-29 14:12:27 +0300

    add a task solution

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+from collections import Counter
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = 'https://greenatom.ru/'
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/108.0.0.0 YaBrowser/23.1.1.1135 Yowser/2.5 Safari/537.36'}
 
+request = requests.get(url, headers=header)
+print(f'Результат выполнения Get запроса: {request.ok}')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if request.ok:
+    page = request.text
+    soup = BeautifulSoup(page, 'html.parser')
+    counter = Counter([tag.name for tag in soup.find_all(True)])
+    print(f'Колличество Html-тегов в коде: {sum(counter.values())}')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    tags_with_attributes = 0
+    for key, val in counter.items():
+        for i in range(val):
+            if soup.find_all(key)[i].attrs:
+                tags_with_attributes += 1
+    print(f'Колличество Html-тегов с атрибутами: {tags_with_attributes}')
