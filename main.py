f4b5dc3d25e861d6e76aa14f2698b9075d541864
Sergey <sergey-petric@yandex.ru>
2023-01-29 14:14:27 +0300

    fix misspell

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ if request.ok:
     page = request.text
     soup = BeautifulSoup(page, 'html.parser')
     counter = Counter([tag.name for tag in soup.find_all(True)])
-    print(f'Колличество Html-тегов в коде: {sum(counter.values())}')
+    print(f'Количество Html-тегов в коде: {sum(counter.values())}')
 
     tags_with_attributes = 0
     for key, val in counter.items():
         for i in range(val):
             if soup.find_all(key)[i].attrs:
                 tags_with_attributes += 1
-    print(f'Колличество Html-тегов с атрибутами: {tags_with_attributes}')
+    print(f'Количество Html-тегов с атрибутами: {tags_with_attributes}')
